a0893c57f2531ba994f8978a502ee2c613970851
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-10 19:22:38 -0300

    feat: Refactor create trip modal component

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -2,11 +2,10 @@ import { X, User, Mail } from "lucide-react";
 
 interface ConfirmTripModalProps {
     closeConfirmTipModal: () => void;
-    addNewEmailToInvite: (event: React.FormEvent<HTMLFormElement>) => void;
-    createTrip: () => void;
+    createTrip: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
-export function ConfirmTripModal({addNewEmailToInvite, closeConfirmTipModal, createTrip}: ConfirmTripModalProps) {
+export function ConfirmTripModal({ closeConfirmTipModal, createTrip}: ConfirmTripModalProps) {
     return (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
           <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -31,7 +30,7 @@ export function ConfirmTripModal({addNewEmailToInvite, closeConfirmTipModal, cre
                 preecha seus dados abaixo:
               </p>
             </div>
-            <form onSubmit={addNewEmailToInvite} className="space-y-3">
+            <form onSubmit={createTrip} className="space-y-3">
               <div className="flex items-center gap-2 h-14 px-5 bg-zinc-950 border border-zinc-800 rounded-lg">
                 <User className="size-5 text-zinc-400" />
                 <input
@@ -49,7 +48,7 @@ export function ConfirmTripModal({addNewEmailToInvite, closeConfirmTipModal, cre
                 />
               </div>
               <button
-                onClick={createTrip}
+               
                 type="submit"
                 className="bg-lime-300 text-lime-950 hover:bg-lime-400 px-5 h-12 gap-2 w-full rounded-lg font-medium flex items-center justify-center"
               >
